302470f1e3373531da3d4270c46ee33033f3d6b8
STORM533 <sakshamkr2006@gmail.com>
2026-09-11 17:01:22 +0530

    corrected middleware ordering

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -1,32 +1,43 @@
 import { Router } from "express";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import {
   createComment,
   deleteComment,
   getComments,
   updateComment,
 } from "../controllers/comments.controller.js";
+import { asyncHandler } from "../middleware/asyncHandler.js";
 import { authenticateJWT } from "../middleware/auth.js";
 import {
   canDeleteComment,
   requireCommentOwner,
 } from "../middleware/commentAuth.js";
+import { optionalAuthenticateJWT } from "../middleware/optionalAuth.js";
 import { validateRequest } from "../middleware/validateRequest.js";
 import type {
   CommentBody,
   CommentParams,
   PostCommentParams,
 } from "../types/comments.js";
-import { asyncHandler } from "../middleware/asyncHandler.js";
-import { optionalAuthenticateJWT } from "../middleware/optionalAuth.js";
 
 const commentsRouter = Router();
 
-commentsRouter.get<PostCommentParams>("/posts/:postId/comments",optionalAuthenticateJWT, getComments);
+commentsRouter.get<PostCommentParams>(
+  "/posts/:postId/comments",
+  optionalAuthenticateJWT,
+  param("postId")
+    .isInt({ min: 1 })
+    .withMessage("Post ID must be a positive integer"),
+  validateRequest,
+  asyncHandler(getComments),
+);
 
 commentsRouter.post<PostCommentParams, object, CommentBody>(
   "/posts/:postId/comments",
   authenticateJWT,
+  param("postId")
+    .isInt({ min: 1 })
+    .withMessage("Post ID must be a positive integer"),
   body("content")
     .isString()
     .withMessage("Content must be a string")
@@ -43,7 +54,8 @@ commentsRouter.post<PostCommentParams, object, CommentBody>(
 commentsRouter.patch<CommentParams, object, CommentBody>(
   "/comments/:id",
   authenticateJWT,
-  requireCommentOwner,
+  param("id").isInt({ min: 1 }).withMessage("ID must be a positive integer"),
+
   body("content")
     .isString()
     .withMessage("Content must be a string")
@@ -53,13 +65,15 @@ commentsRouter.patch<CommentParams, object, CommentBody>(
     .withMessage("Content cannot be empty"),
 
   validateRequest,
-
+  requireCommentOwner,
   asyncHandler(updateComment),
 );
 
 commentsRouter.delete<CommentParams>(
   "/comments/:id",
   authenticateJWT,
+  param("id").isInt({ min: 1 }).withMessage("ID must be a positive integer"),
+  validateRequest,
   canDeleteComment,
   asyncHandler(deleteComment),
 );
