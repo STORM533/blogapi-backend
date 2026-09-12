adaaea5f5a4ddcacb85cc70daaf1b08dbd7ee156
STORM533 <sakshamkr2006@gmail.com>
2026-09-12 20:56:03 +0530

    increased limit for rate limiting global endpoints

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 50,
+  limit: 200,
   standardHeaders: "draft-8",
   legacyHeaders: false,
   message: {
